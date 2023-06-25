bd381e5710b1b6235b1aff6997facab1e1fa8017
viplazylmht <viplazylmht@gmail.com>
2023-06-25 21:04:17 +0700

    add hot reload

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 from typing import Union
 
 from fastapi import FastAPI
+from openapi_tags import tags_metadata
 
-app = FastAPI()
+app = FastAPI(openapi_tags=tags_metadata)
 
-
-@app.get("/")
+@app.get("/", tags=["root"])
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/ping", tags=["ping"])
+def ping_pong():
+    return "pong!"
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
---
+++ app/openapi_tags.py
@@ -0,0 +1,18 @@
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+    {
+        "name": "ping",
+        "description": "An example of a (dumb) ping pong operation.",
+    },
+]
