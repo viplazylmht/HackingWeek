377d67a919588bb8d2a3d088a7b64325aacad4cb
viplazylmht <viplazylmht@gmail.com>
2023-06-29 09:45:59 +0700

    add cache

--- app/miniapp_journey.py
+++ app/miniapp_journey.py
@@ -31,6 +31,17 @@ def build_root_node(start_date: date, granularity: Optional[Granularity], start_
 def get_top_journeys_from_node(start_date: date, granularity: Optional[Granularity], start_node: Optional[str] = None, device_os: Optional[str] = None):
 
     date_time = datetime(year=start_date.year, month=start_date.month, day=start_date.day,)
+
+    # retrieve paths from cache
+
+    top_journeys_cache_collection = journey_db["top_journeys_cache"]
+
+    top_journeys = top_journeys_cache_collection.find_one({"date": date_time, "granularity": granularity.value, "start_node": start_node, "device_os": device_os})
+
+    if top_journeys:
+        return top_journeys['data']
+
+
     root_node = build_root_node(start_date, granularity, start_node, device_os)
 
     # TODO: retrieve paths from cache
@@ -51,13 +62,24 @@ def get_top_journeys_from_node(start_date: date, granularity: Optional[Granulari
     # sort the result by sessions, descending
     result.sort(key=lambda k: (k["stats"]["sessions"], k["stats"]["dist_users"]), reverse=True)
 
+    # save top journeys to cache
+    top_journeys_cache_collection.insert_one({"date": date_time, "granularity": granularity.value, "start_node": start_node, "device_os": device_os, "data": result})
+
     # result set limit to 1000
     return result[:1000]
 
 def get_first_nodes(ds: date, granularity: Optional[Granularity], device_os: Optional[str] = None):
 
-    root_filter = {}
     date_time = datetime(year=ds.year, month=ds.month, day=ds.day,)
+    # retrieve first nodes from cache
+    first_node_cache_collection = journey_db["first_node_cache"]
+
+    first_node = first_node_cache_collection.find_one({"date": date_time, "granularity": granularity.value, "device_os": device_os})
+
+    if first_node:
+        return first_node['data']
+
+    root_filter = {}
 
     if granularity == Granularity.DAILY:
         root_filter["journey_date"] = {"$gte": date_time, "$lt": date_time + timedelta(days=int(Granularity.DAILY.value))}
@@ -77,9 +99,22 @@ def get_first_nodes(ds: date, granularity: Optional[Granularity], device_os: Opt
         if path['path']['entity_name'] not in result:
             result.append(path['path']['entity_name'])
 
+    # save first nodes to cache
+    first_node_cache_collection.insert_one({"date": date_time, "granularity": granularity.value, "device_os": device_os, "data": result})
+
     return [{"node_name": node_name} for node_name in result]
 
 def get_path_tree(start_date: date, granularity: Granularity,  node_name: Union[str, None] = None, depth: Union[int, None] = 0, device_os: Union[str, None] = None):
+
+    date_time = datetime(year=start_date.year, month=start_date.month, day=start_date.day,)
+    # retrieve paths from cache
+
+    path_tree_cache_collection = journey_db["path_tree_cache"]
+
+    path_tree = path_tree_cache_collection.find_one({"date": date_time, "granularity": granularity.value, "node_name": node_name, "depth": depth, "device_os": device_os})
+
+    if path_tree:
+        return Response(content=json.dumps(path_tree['data']), status_code=status.HTTP_200_OK, media_type="application/json")
 
     if not node_name and depth > 0:
         return Response(status_code=status.HTTP_400_BAD_REQUEST, content="node_name is required if depth > 0")
@@ -91,7 +126,6 @@ def get_path_tree(start_date: date, granularity: Granularity,  node_name: Union[
         return Response(status_code=status.HTTP_400_BAD_REQUEST, content="depth must be > 0 if node_name is provided")
 
     root_node = build_root_node(start_date, granularity, None, device_os) # root node do not have node_name, pass None instead
-    date_time = datetime(year=start_date.year, month=start_date.month, day=start_date.day,)
 
     # TODO: retrieve paths from cache
 
@@ -111,4 +145,8 @@ def get_path_tree(start_date: date, granularity: Granularity,  node_name: Union[
     transform_and_limit_tree(tree)
 
     result = tree
+
+    # save path tree to cache
+    path_tree_cache_collection.insert_one({"date": date_time, "granularity": granularity.value, "node_name": node_name, "depth": depth, "device_os": device_os, "data": result})
+
     return Response(content=json.dumps(result), status_code=status.HTTP_200_OK, media_type="application/json")
